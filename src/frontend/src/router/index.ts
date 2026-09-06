d05824b9108d294801d4b5f300ed4499716e4d3a
declineaberdeen <declineaberdeen@foxmail.com>
2026-09-06 08:43:47 +0800

    feat: route read-listen multi-track studio

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -11,6 +11,7 @@ const routes:RouteRecordRaw[]=[
       {path:'novels',component:()=>import('../views/NovelLibraryView.vue')},
       {path:'novels/:id',component:()=>import('../views/NovelWorkspaceView.vue')},
       {path:'production',component:()=>import('../views/ProductionStudioView.vue')},
+      {path:'production-daw',component:()=>import('../views/ProductionDawView.vue')},
       {path:'jobs',component:()=>import('../views/JobCenterView.vue')},
       {path:'styles',component:()=>import('../views/WritingStyleView.vue')},
       {path:'writing',component:()=>import('../views/WritingStudioView.vue')},
